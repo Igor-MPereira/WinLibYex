5d6b50e2dc7b54958b89b015a5176d496b1be7d3
Igor-MPereira <51838741+Igor-MPereira@users.noreply.github.com>
2021-12-18 18:57:26 -0300

    update script

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,6 +1,7 @@
 $RepoPath = "https://raw.githubusercontent.com/Igor-MPereira/WinLibYex/master"
 $OutPath = "./winlib"
 
+New-Item "$($OutPath)" -ItemType Directory -ea 0
 Invoke-WebRequest "$($RepoPath)/pwsh.yex" -OutFile "$($OutPath)/pwsh.yex"
 Invoke-WebRequest "$($RepoPath)/cmd.yex" -OutFile "$($OutPath)/cmd.yex"
 Invoke-WebRequest "$($RepoPath)/winsystem.yex" -OutFile "$($OutPath)/winsystem.yex"
